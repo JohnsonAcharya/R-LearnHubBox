678f72b8bd972fec940f503e6408b03a883f34d9
GaneshAc <ganesh.vicino@gmail.com>
2025-06-03 19:33:51 +0530

    Added - Updated guidesteps

--- Git setup/git for R user.R
+++ Git setup/git for R user.R
@@ -1,3 +1,20 @@
 ## Steps to use GitHub in R
 
 # If already installed Git, Setup Done
+
+
+install.packages("usethis")
+library(usethis)
+
+
+use_git()  # Next follow the steps
+
+# Select the option
+
+library(gitcreds)
+gitcreds_set()
+ 
+## Sbort with error or replace the credentials with existing or replace with updated token from github password
+
+use_github()
+#this will create Github repo with your project Name in Github
