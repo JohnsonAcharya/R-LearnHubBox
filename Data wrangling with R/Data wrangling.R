67f7dc73c9a8d83c78573d85e752359206310784
GaneshAc <ganesh.vicino@gmail.com>
2025-06-06 14:51:30 +0530

    added select function to select specific column

--- Data wrangling with R/Data wrangling.R
+++ Data wrangling with R/Data wrangling.R
@@ -9,6 +9,9 @@ view(diamonds)
 
 #  Subset by row with filter()----------
 
+?filter
+
+#filer row from specific column by value or char
 diamond_sm <- filter(diamonds, price > 1000)
 diamond_sm
 
@@ -24,3 +27,34 @@ diamond_sm
 #Checks missing value
 diamond_sm <- is.na(diamonds)
 
+# --------------------------------------------------------------------------------
+# Subset by column with select()---------------
+
+?select
+
+# Select specific column from datasets
+diamond_sm <- select(diamonds, color,cut) 
+diamond_sm
+
+# Select column from start 1 to 4 from dataset
+diamond_sm <- select(diamonds, 1:4)
+diamond_sm
+
+
+# Select column which column names starts with C
+diamond_sm <- select(diamonds, starts_with("c"))
+diamond_sm
+
+# Select column which column names contain any specific latter  "C"
+diamond_sm <- select(diamonds, contains("t"))
+diamond_sm
+
+
+# Select a column you want as first column with all columns from datasets
+diamond_sm <- select(diamonds, price, everything())
+diamond_sm
+
+#Remove specific column from dataset using - before coumn name
+diamond_sm <- select(diamonds, -price)
+diamond_sm
+#--------------------------------------------------------------------------------
