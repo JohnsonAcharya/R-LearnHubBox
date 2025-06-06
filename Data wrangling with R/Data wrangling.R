c2d4fc59028f3306f138b05981f05904d265824d
GaneshAc <ganesh.vicino@gmail.com>
2025-06-06 14:28:09 +0530

    Added Subset by filter function

--- Data wrangling with R/Data wrangling.R
+++ Data wrangling with R/Data wrangling.R
@@ -5,3 +5,22 @@ library(tidyverse)
 
 ?diamonds
 view(diamonds)
+
+
+#  Subset by row with filter()----------
+
+diamond_sm <- filter(diamonds, price > 1000)
+diamond_sm
+
+diamond_sm <- filter(diamonds, cut == "Ideal")
+diamond_sm
+
+
+diamond_sm <- filter(diamonds,
+                     cut == "Ideal",
+                     price >10000)
+diamond_sm
+
+#Checks missing value
+diamond_sm <- is.na(diamonds)
+
