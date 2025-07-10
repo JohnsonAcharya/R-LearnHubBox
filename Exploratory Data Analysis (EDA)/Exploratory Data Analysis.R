325f70a441c9a389ece59962f07960bcf702de56
GaneshAc <ganesh.vicino@gmail.com>
2025-07-10 19:36:54 +0530

    Added Exploratory Data Analysis (EDA)

---
+++ Exploratory Data Analysis (EDA)/Exploratory Data Analysis.R
@@ -0,0 +1,13 @@
+### Step-by-Step Guide to Analyze Data in R
+
+##   Step 1: Load Required Libraries
+
+library(tidyverse)   # For data manipulation and visualization
+library(readr)       # For reading CSV or text files
+library(ggplot2)     # For plotting
+library(dplyr)       # For data wrangling
+library(skimr)       # For data overview
+library(janitor)     # For cleaning column names
+
+
+
